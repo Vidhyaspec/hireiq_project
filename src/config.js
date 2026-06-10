eb3871e14440153ec331828ddd5d93d6eda658cb
Vidhyaspec <vidhyasri0615@gmail.com>
2026-06-11 02:05:40 +0530

    Update API_URL for production environment

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const API_URL =
   import.meta.env.VITE_API_URL ||
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8080/hireiq-project/backend/api"
-    : "https://hireiq.infinityfreeapp.com/backend/api");
+    : "https://hireiq-project-deploy.onrender.com/backend/api");
 
 export default API_URL;
